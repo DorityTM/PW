630490eb9e17bdd4858bf34e3f799464a0c71dfe
DorityTM <tatyana.mih.davidyuk@gmail.com>
2025-10-26 00:15:48 +0200

    Resolved  suggestions: added timeout constants & AAA approach

--- PW_project/src/tests/ui/sales-portal/products/HW22_tesk1&2.spec.ts
+++ PW_project/src/tests/ui/sales-portal/products/HW22_tesk1&2.spec.ts
@@ -14,37 +14,52 @@ import { HomePage } from "ui/pages/home.page";
 import { LoginPage } from "ui/pages/login.page";
 import { AddNewProductPage } from "ui/pages/products/addNewProduct.page";
 import { ProductsListPage } from "ui/pages/products/productsList.page";
+import { TIMEOUT_5_S, TIMEOUT_10_S } from "data/salesPortal/constants";
 import _ from "lodash";
 
+let productData: ReturnType<typeof generateProductData>;
+
+test.beforeEach(async () => {
+  // ARRANGE: generate product data
+  productData = generateProductData();
+});
+
 test("COMPARE NEW PRODUCT WITH DATA IN TABLE", async ({ page }) => {
+  // ARRANGE: initialize pages
     const loginPage = new LoginPage(page);
     const homePage = new HomePage(page);
     const productsListPage = new ProductsListPage(page);
     const addNewProductPage = new AddNewProductPage(page);
 
+    // ACT: login
     await homePage.open(); 
-    await expect(loginPage.uniqueElement).toBeVisible({timeout: 6000});
+    await expect(loginPage.uniqueElement).toBeVisible({ timeout: TIMEOUT_5_S });
     await loginPage.fillCredentials(credentials.username, credentials.password);
     await loginPage.loginButtonClick();
   
-  
+    // ACT: create a new product
     await homePage.waitForOpened();
     await homePage.clickOnViewModule("Products");
+
     await productsListPage.waitForOpened();
     await productsListPage.clickAddNewProduct();
+    
     await addNewProductPage.waitForOpened();
-    const productData = generateProductData();
     await addNewProductPage.fillForm(productData);
     await addNewProductPage.clickSave();
+    
+    // ASSERT: verify notification and appearing the product in the table
     await productsListPage.waitForOpened();
-    await expect(productsListPage.toastMessage).toContainText(NOTIFICATIONS.PRODUCT_CREATED);
+    await expect(productsListPage.toastMessage).toContainText(NOTIFICATIONS.PRODUCT_CREATED, { timeout: TIMEOUT_10_S });
     await expect(productsListPage.tableRowByName(productData.name)).toBeVisible();
 
+    // ASSERT: verify product data in the table
     const productFromTable = await productsListPage.getProductData(productData.name);
     const expectedProduct = _.omit(productData, ["notes", "amount"]);
     const actualProduct = _.omit(productFromTable, ["createdOn"]);
     expect(actualProduct).toEqual(expectedProduct);
 
+    // LOGGING the created product data
     const newProduct = await productsListPage.getProductData(productData.name);
     console.log(`Created product: ${JSON.stringify(newProduct)}`);
   });
